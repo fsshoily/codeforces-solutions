b750e3e5d4214bc6b1b003c45009a87885929f44
fsshoily <farihasabrinshoily@gmail.com>
2025-07-07 21:49:02 +0800

    Sheet 02-D_Fixed_Password

---
+++ D_Fixed_Password.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int X;
+    while (cin>>X) {  
+      if(X==1999)
+    {
+        cout<<"Correct";
+        break ;
+    }
+    else 
+    { 
+     cout<<"Wrong"<<'\n';
+    }
+} 
+return 0;
+}
