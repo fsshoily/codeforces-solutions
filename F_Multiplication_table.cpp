f5352ba4d6951df7561181b39e102d08c61bf6be
fsshoily <farihasabrinshoily@gmail.com>
2025-07-08 09:10:38 -0400

    Sheet 02-
    F_Multiplication_table

---
+++ F_Multiplication_table.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N,i,s;
+    cin>>N;
+    for(i=1;i<=12;i++)
+    {
+       s= N*i;
+       cout<<N<<" "<<"*"<<" "<<i<<" "<<"="<<" "<<s<<'\n';
+    }
+    return 0;
+}
