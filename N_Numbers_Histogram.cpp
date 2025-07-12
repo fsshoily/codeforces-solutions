b41f9c5458399d4748fa1bff14e43eb1cd91df04
fsshoily <farihasabrinshoily@gmail.com>
2025-07-12 17:32:40 +0600

    Sheet 02-N_Numbers_Histogram

---
+++ N_Numbers_Histogram.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char S;
+    int N;
+    cin>>S>>N;
+    for(int i=0;i<N;i++)
+    {
+        int x;
+       cin>>x;
+          for(int j=0;j<x;j++)
+          {
+            cout<<S;
+          }
+          cout<<endl;
+    }
+}
