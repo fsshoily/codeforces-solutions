af113fe4e7e8c7aebfc7c9adf5d1e38639204f65
fsshoily <farihasabrinshoily@gmail.com>
2025-07-14 22:27:16 +0600

    Sheet 02 -W_Shape_3

---
+++ W_Shape_3.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cin>>N;
+    for(int i = 1; i <= N; i++)
+    {
+        for(int j = 0 ; j < N-i ; j++)
+        {
+            cout<<" ";
+        }
+        for (int k = 0; k <2 * i - 1; k++)
+         {
+        cout << "*";
+         }
+        cout<<endl;
+    }
+    for(int l = N; l >= 1; l--)
+    {
+        for(int m = 0 ; m < N-l ; m++)
+        {
+            cout<<" ";
+        }
+        for (int n = 0; n <2 * l - 1; n++)
+         {
+        cout <<"*";
+         }
+        cout<<endl;
+    }
+
+}
