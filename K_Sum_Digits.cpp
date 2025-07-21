10e4569e77969c4878986e9ea2b8cb0ae47496b1
fsshoily <farihasabrinshoily@gmail.com>
2025-07-21 17:08:28 +0600

    Sheet 03-K_Sum_Digits

---
+++ K_Sum_Digits.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int N; cin>>N;
+    string A;
+    cin>>A;
+    int sum=0;
+    for(int i=0; i<N; i++)
+    {
+        sum+=A[i] - '0';
+    }
+    cout<<sum;
+}
