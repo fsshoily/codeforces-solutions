75ef5f8c98220a9c28228e47af70ef86616dbe40
fsshoily <farihasabrinshoily@gmail.com>
2025-08-08 22:18:22 +0600

    Sheet 04-J_Count_Letters

---
+++ J_Count_Letters.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string S;
+    cin>>S;
+    int freq[26]={0};
+    for (char c:S) {
+        freq[c-'a']++;
+    }
+    for (int i=0; i<26; i++) {
+        if (freq[i]>0) {
+            char ch='a'+i;
+            cout<<ch<<" : "<<freq[i]<<endl;
+        }
+    }
+}
