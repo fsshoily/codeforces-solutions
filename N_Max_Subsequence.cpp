941088618143f20e1971029daaac32b3d7b7518d
fsshoily <farihasabrinshoily@gmail.com>
2025-08-11 21:34:05 +0600

    Sheet 04-N_Max_Subsequence

---
+++ N_Max_Subsequence.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int N;
+    string S;
+    cin>>N>>S;
+    int count = 1; 
+    for (int i=1; i<N; i++) {
+        if (S[i]!= S[i-1]) {
+            count++;
+        }
+    }
+
+    cout<<count<<endl;  
+}
